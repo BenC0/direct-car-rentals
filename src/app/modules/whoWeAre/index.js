622feddc0a9a3dad85cbb01af997c37112fa00f4
Benji Cohen <suddensquirrel94@gmail.com>
2023-08-20 10:27:12 +0100

    Who We Are desktop styling

--- src/app/modules/whoWeAre/index.js
+++ src/app/modules/whoWeAre/index.js
@@ -6,9 +6,9 @@ import { CallToAction } from "../call_to_action"
 export function WhoWeAre(props) {
 	return  (
         <section className={`${styles.WhoWeAre} content-wrapper`}>
-            <div className='content a-center j-center column'>
+            <div className={`${styles.WhoWeAreContent} content a-center j-center column`}>
                 <h3 className={`${styles.WhoWeAreTitle} ${oswald.className}`}>Who We Are</h3>
-                <div className={styles.WhoWeAreContent}>
+                <div className={styles.WhoWeAreCopy}>
                     <ul className={`${ptsans.className}`}>
                         <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
                         <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedr in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
