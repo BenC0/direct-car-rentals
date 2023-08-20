8e13e860ffce0041ef14471cd9c23c09a6b87833
Benji Cohen <suddensquirrel94@gmail.com>
2023-08-20 09:42:47 +0100

    Restructure navigation to use global styles

--- src/app/modules/navigation/index.js
+++ src/app/modules/navigation/index.js
@@ -4,12 +4,14 @@ import { CallToAction } from "../call_to_action"
 
 export function Navigation() {
 	return  (
-        <div className={styles.Navigation}>
-            <h1 className={`${oswald.className} ${styles.logo}`}>Direct Car Rentals</h1>    
-            <CallToAction
-                text="Get in Touch"
-                href="tel:01617980000"
-            ></CallToAction>
+        <div className={`${styles.Navigation} content-wrapper`}>
+            <div className={`${styles.NavigationContent} content`}> 
+                <h1 className={`${oswald.className} ${styles.logo}`}>Direct Car Rentals</h1>    
+                <CallToAction
+                    text="Get in Touch"
+                    href="tel:01617980000"
+                ></CallToAction>
+            </div>
         </div>
     )
 }
