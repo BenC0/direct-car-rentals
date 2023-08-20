102b6d06523592cc209ab88aa830ea88b9684575
Benji Cohen <suddensquirrel94@gmail.com>
2023-08-20 09:47:44 +0100

    Desktop Hero styling and restructure for global layout styles

--- src/app/modules/hero/index.js
+++ src/app/modules/hero/index.js
@@ -1,13 +1,14 @@
 import styles from './index.module.css'
-import { oswald, ptsans } from "../../styles/fonts"
-import { CallToAction } from "../call_to_action"
+import { ptsans } from "../../styles/fonts"
 
 export function Hero() {
 	return  (
-        <div className={styles.Hero}>
-            <h2 className={`${styles.HeroText} ${ptsans.className}`}>Price Match Guarantee*</h2>
-            <h3 className={`${styles.HeroText} ${ptsans.className}`}>Call Today For The Best Possible Quote</h3>
-            <a className={`${styles.HeroText} ${styles.HeroTextLink} ${ptsans.className}`} href="tel:01617980000"><span>0161 761 1234</span></a>
+        <div className={`${styles.Hero} content-wrapper`}>
+            <div className={`${styles.HeroContent} content column a-center`}>
+                <h2 className={`${styles.HeroText} ${ptsans.className}`}>Price Match Guarantee*</h2>
+                <h3 className={`${styles.HeroText} ${ptsans.className}`}>Call Today For The Best Possible Quote</h3>
+                <a className={`${styles.HeroText} ${styles.HeroTextLink} ${ptsans.className}`} href="tel:01617980000"><span>0161 761 1234</span></a>
+            </div>
         </div>
     )
 }
