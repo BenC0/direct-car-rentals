bbf819da66eba44275e779c25aaed8517b67004b
Benji Cohen <suddensquirrel94@gmail.com>
2023-09-12 13:31:34 +0100

    Updated About Us

--- src/app/modules/aboutUs/index.js
+++ src/app/modules/aboutUs/index.js
@@ -10,9 +10,9 @@ export function AboutUs(props) {
                 <div className={`${styles.AboutUsContent} m-column`}>
                     <div className={styles.AboutUsTable}>
                         <p className={styles.AboutUsTableLabel}>Location</p>
-                        <p className={styles.AboutUsTableValue}>Sample House <br />Sample Street <br />Bury <br />BL8 000</p>
+                        <p className={styles.AboutUsTableValue}>Woolfold Industrial Estate<br />ALSTON HOUSE<br />Alston St<br/>Bury<br/>BL8 1SF</p>
                         <p className={styles.AboutUsTableLabel}>Contact</p>
-                        <p className={styles.AboutUsTableValue}>0161 761 1234<br />DirectCarHire@gmail.com</p>
+                        <p className={styles.AboutUsTableValue}>0161 761 7773<br />DirectCarHire@gmail.com</p>
                         <p className={styles.AboutUsTableLabel}>Business Hire</p>
                         <p className={styles.AboutUsTableValue}>Sam Pull</p>
                         <p className={styles.AboutUsTableFooter}>*  All price matches are like for like within the local area.</p>
