ae53c35d0b8d19f2c78617d1b03b5a39cbb5b2a4
Benji Cohen <suddensquirrel94@gmail.com>
2023-09-12 13:29:47 +0100

    Added reviews and reviews link

--- src/app/modules/call_to_action/index.js
+++ src/app/modules/call_to_action/index.js
@@ -3,7 +3,7 @@ import { oswald } from "../../styles/fonts"
 
 export function CallToAction(props) {
 	return  (
-        <a className={styles.CallToAction} href={props.href}>
+        <a className={styles.CallToAction} href={props.href} target="_blank">
             <span className={`${oswald.className}`}>{props.text}</span>    
         </a>
     )
--- src/app/modules/reviews/index.js
+++ src/app/modules/reviews/index.js
@@ -7,32 +7,26 @@ const ReviewsContent = [
     {
         key: 0,
         rating: 5,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan et elit id aliquam. Morbi vel augue id leo semper vehicula. Ut fringilla mauris in neque ullamcorper consequat.",
-        author: "Mrs. Lauren Ipsum, New Zealand"
+        content: "Hired a few times the last month or so whilst ours is off the road; quick in and out with no sales pitch selling extras.  Provide what you need at a good price. Would recommend.",
+        author: "Kathryn Hart"
     },
     {
         key: 1,
-        rating: 4,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan et elit id aliquam. Morbi vel augue id leo semper vehicula. Ut fringilla mauris in neque ullamcorper consequat.",
-        author: "Mrs. Lauren Ipsum, New Zealand"
+        rating: 5,
+        content: "Best car hire amazing people great prices amazing people to talk too 👍 I would certainly recommend this car hire best keep the good work up guy's well done 👏",
+        author: "Richard James"
     },
     {
         key: 2,
-        rating: 3,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan et elit id aliquam. Morbi vel augue id leo semper vehicula. Ut fringilla mauris in neque ullamcorper consequat.",
-        author: "Mrs. Lauren Ipsum, New Zealand"
+        rating: 5,
+        content: "I've hired vans on an ongoing basis now for approximately 6 months from Totty Van Hire. I've found them to be reasonably priced vans, ran by nice people who go out of their way to help...",
+        author: "Sean Swimby"
     },
     {
         key: 3,
-        rating: 2,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan et elit id aliquam. Morbi vel augue id leo semper vehicula. Ut fringilla mauris in neque ullamcorper consequat.",
-        author: "Mrs. Lauren Ipsum, New Zealand"
-    },
-    {
-        key: 4,
-        rating: 1,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan et elit id aliquam. Morbi vel augue id leo semper vehicula. Ut fringilla mauris in neque ullamcorper consequat.",
-        author: "Mrs. Lauren Ipsum, New Zealand"
+        rating: 5,
+        content: "Hired Ford Focus for 2 days, 2 drivers; £80. Marvellous. Really enjoyed the experience. No hidden extras like most other companies. Know where to go next time",
+        author: "Brian Pattila"
     },
 ]
 
@@ -55,7 +49,7 @@ export function Reviews(props) {
                 <div className={`${styles.ButtonContainer} j-center`}>
                     <CallToAction
                         text="Read more reviews"
-                        href="#reviews-link"
+                        href="https://www.google.com/search?client=ms-android-samsung-ss&sca_esv=560305232&cs=0&output=search&q=Direct+Car+and+Van+Rentals&ludocid=9518490544439094226&lsig=AB86z5VUdgnZ7Vo6ACMd-RkcWCWJ&kgs=ce6bae3e293fa750&shndl=-1&shem=lbsc,lsp&source=sh/x/kp/local/m1/2#lrd=0x487ba4f69e60323d:0x841878b81334ebd2,1,,,,"
                     ></CallToAction>
                 </div>
             </div>
