a1801452fbd95d2dadc7f002256cb3a0d3056af3
Benji Cohen <suddensquirrel94@gmail.com>
2023-09-12 13:31:56 +0100

    Updated phone number in Hero

--- src/app/modules/hero/index.js
+++ src/app/modules/hero/index.js
@@ -7,7 +7,7 @@ export function Hero() {
             <div className={`${styles.HeroContent} content column a-center`}>
                 <h2 className={`${styles.HeroText} ${ptsans.className}`}>Price Match Guarantee*</h2>
                 <h3 className={`${styles.HeroText} ${ptsans.className}`}>Call Today For The Best Possible Quote</h3>
-                <a className={`${styles.HeroText} ${styles.HeroTextLink} ${ptsans.className}`} href="tel:01617980000"><span>0161 761 1234</span></a>
+                <a className={`${styles.HeroText} ${styles.HeroTextLink} ${ptsans.className}`} href="tel:01617617773"><span>0161 761 7773</span></a>
             </div>
         </div>
     )
